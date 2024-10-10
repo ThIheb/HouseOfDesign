16cdd334b60bd5142eada4957eac4415a3c173c2
VirginiaDa00 <virginia.dantonio@studio.unibo.it>
2024-10-10 15:14:21 +0200

    timeline changes

--- narratives/narratives.js
+++ narratives/narratives.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function showTime(e) {
       e.setAttribute("done", "true");
-      e.querySelector(".timeline-point").style.background = "blue";
+      e.querySelector(".timeline-point").style.background = "#F50045";
       e.querySelector(".date").style.opacity = "100%";
       e.querySelector("p").style.opacity = "100%";
       e.querySelector("p").style.transform = "translateY(0px)";
