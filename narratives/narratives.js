3578461ba361381b9f5531ca4d04f0dfff720282
VirginiaDa00 <virginia.dantonio@studio.unibo.it>
2024-10-16 15:30:33 +0200

    update narrative.js no timeline

--- narratives/narratives.js
+++ narratives/narratives.js
@@ -42,39 +42,7 @@ function updateCarousel() {
 });
 
 
-  updateCarousel();
 
-  document.getElementById('prev').addEventListener('click', function () {
-      moveSlide(-1);
-  });
-
-  document.getElementById('next').addEventListener('click', function () {
-      moveSlide(1);
-  });
-
-  function moveSlide(direction) {
-      currentItem = (currentItem + direction + totalItems) % totalItems; 
-      updateCarousel();
-  }
-
-  function updateCarousel() {
-      const percentage = -(currentItem * 100); 
-      designers.style.transform = `translateX(${percentage}%)`;
-      
-    
-      items.forEach((item, index) => {
-          const intro = item.querySelector('.intro');
-          if (index === currentItem) {
-              item.style.zIndex = 10;
-              intro.style.opacity = '1';
-              intro.style.pointerEvents = 'auto';
-          } else {
-              item.style.zIndex = 5;
-              intro.style.opacity = '0';
-              intro.style.pointerEvents = 'none';
-          }
-      });
-  }
 
 
 /*to make the content appear in a cool way*/
